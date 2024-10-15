b3b1380b622819abc399b559e794d1646d39ed2e
tenyega <tenyega23@gmail.com>
2024-10-15 22:34:32 +0200

    EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/ReservationCrudController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Reservation;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class ReservationCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Reservation::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+
+            DateTimeField::new('startDate'),
+            DateTimeField::new('endDate'),
+            TimeField::new('startTime'),
+            TimeField::new('endTime'),
+            BooleanField::new('isConfirmed'),
+            TextareaField::new('specialRequest'),
+            AssociationField::new('userId')
+                ->setLabel('User') // Set a label for the field
+                ->setFormTypeOption('choice_label', function (?User $user) {
+                    return $user ? $user->getEmail() : 'No user'; // For forms
+                })
+                ->setCustomOption('choice_label', function (?User $user) {
+                    return $user ? $user->getEmail() : 'No user'; // For display in lists
+                }),
+        ];
+    }
+}
