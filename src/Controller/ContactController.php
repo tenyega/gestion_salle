bb7362a533a46b9113746aefce480f8ea0ea5e61
tenyega <tenyega23@gmail.com>
2024-10-18 17:01:17 +0200

    contact page upadated

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Address;
-use App\Repository\AddressRepository;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Address;
+use App\Security\EmailVerifier;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,12 +15,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
-    
+    public function __construct(private EmailVerifier $emailVerifier, private EntityManagerInterface $em) {}
     #[Route('', name: 'app_contact', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('contact/index.html.twig');
-       
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $this->getUser(),
+                (new TemplatedEmail())
+                    ->from(new Address('hall4all@email.com', 'hall4all'))
+                    ->to((string) $this->getUser()->getEmail())
+                    ->subject('Thank you for contacting us ')
+                    ->htmlTemplate('contact/confirmation.html.twig')
+            );
+            $this->em->flush();
+            return $this->redirectToRoute('app_reservation_index');
+        }
+
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form,
+        ]);
     }
-   
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Your Name',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Your Email',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Subject',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => ['class' => 'form-control', 'rows' => 5],
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Send',
+                'attr' => ['class' => 'btn btn-primary'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null, // Pas d'entité associée
+        ]);
+    }
+}
