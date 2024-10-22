4a8bef594d1a19f812ee269653158cca1a1b4e91
tenyega <tenyega23@gmail.com>
2024-10-22 11:25:11 +0200

    reservationType update

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -11,10 +11,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Mime\Message;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Time;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class ReservationType extends AbstractType
 {
@@ -28,6 +32,10 @@ class ReservationType extends AbstractType
                     new NotBlank([
                         'message' => 'Please enter a start date.',
                     ]),
+                    new GreaterThanOrEqual([
+                        'value' => new \DateTime(),  // Ensures the date is greater than or equal to today's date
+                        'message' => 'The date must be today or later.',
+                    ])
 
                 ],
             ])
@@ -38,6 +46,7 @@ class ReservationType extends AbstractType
                     new NotBlank([
                         'message' => 'Please enter an end date.',
                     ]),
+
                 ],
             ])
             ->add('startTime', TimeType::class, [
@@ -62,7 +71,21 @@ class ReservationType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Reservation::class,
+            'data_class' => Reservation::class,  // Assuming your form is for the Reservation entity
+            'constraints' => [
+                new Callback([$this, 'validateDates']),
+            ],
         ]);
+    }
+    public function validateDates($reservation, ExecutionContextInterface $context)
+    {
+        $startDate = $reservation->getStartDate();
+        $endDate = $reservation->getEndDate();
+
+        if ($endDate < $startDate) {
+            $context->buildViolation('The end date must be greater than or equal to the start date.')
+                ->atPath('endDate')
+                ->addViolation();
+        }
     }
 }
